c8b9c129d3639029f82e96f1e260359b6edebc3e
“Filippo <filippo.kansikas@refresh-academy.org>
2025-07-02 15:15:07 +0200

    added refresh colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-amber-700'>
+      <div className='bg-refresh-pink'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
