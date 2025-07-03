87a2ee566762ee240263e162138e949d281251f1
Enrico-M-B <enrico.moscariello@refresh-academy.org>
2025-07-03 15:53:26 +0200

    Implement initial navbar WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router";
+import  logorefreChef from '/logorefreChef.png'
+import Home from './routes/Home.jsx'
+
+const Layout = () => {
+  return(<div className='min-h-screen flex flex-col'>
+    <div className='flex flex-row bg-white'>
+        <Link to={'/'} className='flex flex-row items-center text-just-eat font-bold gap-2 p-2'>
+          <img src={logorefreChef} className="w-10 h-10" alt="Refrechef-logo" />
+          <p className='text-black'>RefreChef</p>
+        </Link>
+        <Link to = {'/login'}>
+        <p>Login</p></Link>
+      </div>
+  </div>)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className='bg-refresh-pink'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
