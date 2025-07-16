ddd408ad3227924a1eb6c74e0751403e2e69ad6e
Lorenzo Garuti <lorenzo.garuti@refresh-academy.org>
2025-07-16 10:40:18 +0200

    fixed z-index for the logout in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -83,7 +83,7 @@ const Layout = ({ user }) => {
                     Welcome, {user.nickname}!
                   </span>
                   {dropdownOpen && (
-                    <div className="absolute right-0 mt-2 w-32 bg-white border rounded shadow z-10">
+                    <div className=" absolute right-0 mt-2 w-32 bg-white border rounded shadow z-15">
                       <button
                         className="w-full text-left px-4 py-2 text-refresh-blue font-semibold hover:bg-white hover:text-refresh-pink rounded transition"
                         onClick={() => {
